a673e6c242eb1b393cfbef5f0dd478e3c21100ac
Felipyasha <felipe_nascalves@hotmail.com>
2026-01-13 14:11:37 -0300

    feat: lesson 12 - html and css from calculator

--- basic-concepts/src/app/app.ts
+++ basic-concepts/src/app/app.ts
@@ -1,11 +1,10 @@
 import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
-import { HelloWolrdComponent } from "./hellowolrd/helloworld.component";
-import { Mypage } from "./mypage/mypage";
+import { Calculator } from './calculator/calculator';
+
 
 @Component({
   selector: 'app-root',
-  imports: [HelloWolrdComponent, Mypage],
+  imports: [Calculator],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
