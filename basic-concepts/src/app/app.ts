c3119da8978ee3059b5f49aafccd2ba1c7224b1f
Felipyasha <felipe_nascalves@hotmail.com>
2026-01-13 13:43:14 -0300

    feat: lesson 11

--- basic-concepts/src/app/app.ts
+++ basic-concepts/src/app/app.ts
@@ -1,10 +1,11 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HelloWolrdComponent } from "./hellowolrd/helloworld.component";
+import { Mypage } from "./mypage/mypage";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, HelloWolrdComponent],
+  imports: [HelloWolrdComponent, Mypage],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
---
+++ basic-concepts/src/app/mypage/mypage.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-mypage',
+  imports: [],
+  templateUrl: './mypage.html',
+  styleUrl: './mypage.scss',
+})
+export class Mypage {
+
+}
