c707950d0ed0948ac8c3ffaa1316a39d335b7648
Felipyasha <felipe_nascalves@hotmail.com>
2026-01-14 17:37:57 -0300

    feat: lesson 18 - Passing form data to TypeScript

--- basic-concepts/src/app/wish-list/wish-list.ts
+++ basic-concepts/src/app/wish-list/wish-list.ts
@@ -1,11 +1,17 @@
 import { Component } from '@angular/core';
+import { FormsModule } from "@angular/forms";
 
 @Component({
   selector: 'app-wish-list',
-  imports: [],
+  imports: [ FormsModule ],
   templateUrl: './wish-list.html',
   styleUrl: './wish-list.scss',
 })
 export class WishList {
+  item: string = '';
 
+  addItem(){
+    console.log(this.item);
+    this.item = '';      
+  }
 }
