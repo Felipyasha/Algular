4dc8b4e657634a378bec33054b94a5f2ad549160
Felipyasha <felipe_nascalves@hotmail.com>
2026-01-16 09:46:32 -0300

    feat: lesson 24 - Creating routes to access the components

--- crud-angular-material/src/app/app.ts
+++ crud-angular-material/src/app/app.ts
@@ -1,9 +1,12 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { MatToolbarModule } from "@angular/material/toolbar";
+import { Consultation } from "./consultation/consultation";
+import { Register } from "./register/register";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, MatToolbarModule, Consultation, Register],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
