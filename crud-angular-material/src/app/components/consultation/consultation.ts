f0e50d809ef7f4d13823b9134a9065dff4219068
Felipyasha <felipe_nascalves@hotmail.com>
2026-01-17 20:38:20 -0300

    feat: lesson 33/34 - The HTML code for the customer search page has been created

--- crud-angular-material/src/app/components/consultation/consultation.ts
+++ crud-angular-material/src/app/components/consultation/consultation.ts
@@ -1,11 +1,30 @@
 import { Component } from '@angular/core';
 
+import { FlexLayoutModule } from '@angular/flex-layout';
+import { FormsModule } from '@angular/forms';
+
+import { MatCardModule } from '@angular/material/card';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
+import { MatTable } from '@angular/material/table';
+
+import { ClientService } from '../../services/client-service';
+
 @Component({
   selector: 'app-consultation',
-  imports: [],
+  imports: [
+    FlexLayoutModule,
+    MatCardModule,
+    FormsModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatIconModule,
+    MatButtonModule,
+    MatTable
+  ],
   templateUrl: './consultation.html',
   styleUrl: './consultation.scss',
 })
-export class Consultation {
-
-}
+export class Consultation {}
